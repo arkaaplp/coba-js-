7fdb9ceebc510c3e0c6935b4af8cf34b6dadcf78
Your name here <arkarezky540@gmail.com>
2021-08-28 13:57:00 +0800

    coba js 1

---
+++ index.js
@@ -0,0 +1,39 @@
+//console.log("SMKN 10 MAKASSAR");
+
+//console.log("24 AGUSTUS 2004");
+
+//console.log("17 Tahun");
+
+//prompt("hii how are you");
+
+//prompt("Masukkan Asal Daerah Anda");
+
+//alert("Terima Kasih sudah mengisi form");
+
+//alert("hii kamu gimana kabarnya")
+
+//alert("udah makan bakso belum wkakaka")
+
+
+// exercise 1
+function exercise(name, barang) {
+   return "Terima Kasih" + " " + name + " " + "telah membeli produk " + barang;}
+
+console.log(exercise("Asep," , "Rinso kami"));
+
+
+//exercise 2
+
+// const lowercase = (name1, name2) =>  {
+//   return  name1.toLowerCase(name1) + " " +  name2.toLowerCase();
+// }
+
+// console.log(lowercase("aRkA" , "RezKY"));
+
+
+
+
+
+
+
+
